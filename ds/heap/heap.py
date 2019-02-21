0e61a7c457bf0c2099643b40a00fec2f2d5bcaf4
Vladimir Chervanev <vladc@fb.com>
2019-02-20 21:12:29 -0600

    Heap part II - minimum value extraction

--- ds/heap/heap.py
+++ ds/heap/heap.py
@@ -7,6 +7,20 @@ class Heap:
         while node.lessThanParent():
             node = node.swapWithParentAndReturn()
 
+    def pop(self):
+        value = self.minimum()
+        if any(self.items): self.remove(0)
+        return value
+
+    def remove(self, index):
+        node = self.items[index]
+        lastNode = self.items.pop()
+        node.value = lastNode.value
+        if node is None:
+            return None
+        while node and node.biggerThanChildNode():
+            node = node.swapWithSmallestChildNodeAndReturn()
+
     def minimum(self):
         return self.items[0].value if any(self.items) else None
 
@@ -26,6 +40,34 @@ class Heap:
             [parent.value, self.value] = [self.value, parent.value]
             return parent
 
+        def swapWithSmallestChildNodeAndReturn(self):
+            childNode = self.smallestChildNode()
+            [childNode.value, self.value] = [self.value, childNode.value]
+            return childNode
+
+        def biggerThanChildNode(self):
+            node = self.smallestChildNode()
+            return node and node.value < self.value
+
+        def smallestChildNode(self):
+            left = self.leftChild()
+            right = self.rightChild()
+            if left is None or right is None:
+                return left or right
+            elif left.value <= right.value:
+                return left
+            else:
+                return right
+
+        def leftChild(self):
+            return self.safeGet(self.index*2+1)
+
+        def rightChild(self):
+            return self.safeGet(self.index*2+2)
+
+        def safeGet(self, index):
+            return self.items[index] if index < len(self.items) else None
+            
         def parent(self):
             if self.index == 0:
                 return None
--- ds/heap/heap_test.py
+++ ds/heap/heap_test.py
@@ -7,10 +7,32 @@ def heap():
     return Heap()
 
 
-def test_minimum(heap):
-    heap.add(300)
+def test_add_and_pop_sequence(heap):
     heap.add(200)
-    heap.add(250)
-    heap.add(400)
+    assert len(heap.items) == 1
     heap.add(100)
-    assert heap.minimum() == 100
+    assert len(heap.items) == 2
+    heap.add(300)
+    assert len(heap.items) == 3
+    heap.add(50)
+    assert len(heap.items) == 4
+    heap.add(400)
+    assert len(heap.items) == 5
+    v = heap.pop()
+    assert len(heap.items) == 4
+    assert v == 50
+    v = heap.pop()
+    assert len(heap.items) == 3
+    assert v == 100
+    v = heap.pop()
+    assert len(heap.items) == 2
+    assert v == 200
+    v = heap.pop()
+    assert len(heap.items) == 1
+    assert v == 300
+    v = heap.pop()
+    assert len(heap.items) == 0
+    assert v == 400
+    v = heap.pop()
+    assert len(heap.items) == 0
+    assert v == None
